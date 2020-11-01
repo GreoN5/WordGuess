f2c277eb0355ae2afa982e4a0bb4b2a136db4c06
GreoN5 <georgi38@abv.bg>
2020-11-01 16:34:21 +0200

    Minor changes

--- WordGuess/Game.cs
+++ WordGuess/Game.cs
@@ -5,192 +5,194 @@ using System.Text;
 
 namespace WordGuess
 {
-	public class Game
-	{
-		private Player _player;
-		public Words _words;
-		private int _numberOfTries;
-		private string _randomWord;
-		private PlayerContext _playerContext = new PlayerContext();
-		private string _wordInProgress;
+    public class Game
+    {
+        private Player _player;
+        private Words _words;
+        private int _numberOfTries;
+        private string _randomWord;
+        private PlayerContext _playerContext = new PlayerContext();
 
-		public Game(Words words, Player player, int numberOfTries)
-		{
-			this._numberOfTries = numberOfTries;
-			this._words = words;
-			this._player = player;
-		}
+        public Game(Words words, Player player, int numberOfTries)
+        {
+            this._numberOfTries = numberOfTries;
+            this._words = words;
+            this._player = player;
+        }
 
-		public void GuessTheWord()
-		{
-			StringBuilder builder = new StringBuilder();
-			this._randomWord = this._words.GetRandomWord();
+        public void GuessTheWord()
+        {
+            StringBuilder builder = new StringBuilder();
+            this._randomWord = this._words.GetRandomWord();
 
-			int numberOfLetters = this._randomWord.Length - 1;
-			ShowWordWithFirstLetter(this._randomWord);
+            string wordInProgress;
 
-			if (this._numberOfTries < 0)
-			{
-				Console.WriteLine($"Game over! The word was \"{this._randomWord}\" and you didn't guess it!");
+            int numberOfLetters = this._randomWord.Length - 1;
+            ShowWordWithFirstLetter(this._randomWord);
 
-				if (this._player.Points > 0)
-				{
-					this._player.Points--;
+            while (true)
+            {
+                if (this._numberOfTries <= 0)
+                {
+                    Console.WriteLine($"Game over! The word was \"{this._randomWord}\" and you didn't guess it!");
 
-					this._playerContext.Update(_player);
-					this._playerContext.SaveChanges();
-				}
+                    if (this._player.Points > 0)
+                    {
+                        this._player.Points--;
 
-				return;
-			}
-			else
-			{
-				while (this._numberOfTries > 0)
-				{
-					if (numberOfLetters <= 0)
-					{
-						// if there are no letters left then you have won the game
-						Console.WriteLine($"You guessed the word! It is \"{this._randomWord}\"!");
+                        this._playerContext.Update(_player);
+                        this._playerContext.SaveChanges();
+                    }
 
-						this._player.Points += 2;
+                    return;
+                }
+                else
+                {
 
-						this._playerContext.Update(_player);
-						this._playerContext.SaveChanges();
+                    if (numberOfLetters <= 0)
+                    {
+                        // if there are no letters left then you have won the game
+                        Console.WriteLine($"You guessed the word! It is \"{this._randomWord}\"!");
 
-						return;
-					}
-					else
-					{
-						Console.Write($"The word has {numberOfLetters} remaining letters and you have {this._numberOfTries}" +
-							$" more tries to guess it! Guess a letter: ");
-						char letter = char.Parse(Console.ReadLine());
+                        this._player.Points += 2;
 
-						this._wordInProgress = WordInProgress(this._randomWord.ToCharArray(), letter, builder);
+                        this._playerContext.Update(_player);
+                        this._playerContext.SaveChanges();
 
-						if (letter == '0' ||
-							letter == '1' ||
-							letter == '2' ||
-							letter == '3' ||
-							letter == '4' ||
-							letter == '5' ||
-							letter == '6' ||
-							letter == '7' ||
-							letter == '8' ||
-							letter == '9')
-						{
-							Console.WriteLine("Invalid letter! Please try again...");
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write($"The word has {numberOfLetters} remaining letters and you have {this._numberOfTries}" +
+                            $" more tries to guess it! Guess a letter: ");
+                        char letter = char.Parse(Console.ReadLine());
 
-							continue;
-						}
-						else
-						{
-							if (CheckForContainingLetters(this._wordInProgress.ToCharArray(), letter))
-							{
-								numberOfLetters--;
-								ShowProgressOfWord(this._wordInProgress);
-							}
-							else
-							{
-								this._numberOfTries--;
-								ShowProgressOfWord(this._wordInProgress);
-							}
-						}
-					}
-				}
-			}
-		}
+                        wordInProgress = WordInProgress(this._randomWord.ToCharArray(), letter, builder);
 
-		public void AddNewWordToTheGame(string word)
-		{
-			bool containsDigit = word.Any(char.IsDigit); // the word must not have any digits
+                        if (letter == '0' ||
+                            letter == '1' ||
+                            letter == '2' ||
+                            letter == '3' ||
+                            letter == '4' ||
+                            letter == '5' ||
+                            letter == '6' ||
+                            letter == '7' ||
+                            letter == '8' ||
+                            letter == '9')
+                        {
+                            Console.WriteLine("Invalid letter! Please try again...");
 
-			if (containsDigit == false && word.Length >= 3) // the word must be at least 3 letters long to be considered as a valid word
-				this._words.ListOfWords.Add(word);
-		}
+                            continue;
+                        }
+                        else
+                        {
+                            if (CheckForContainingLetters(wordInProgress.ToCharArray(), letter))
+                            {
+                                numberOfLetters--;
+                                ShowProgressOfWord(wordInProgress);
+                            }
+                            else
+                            {
+                                this._numberOfTries--;
+                                ShowProgressOfWord(wordInProgress);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		private bool CheckForContainingLetters(char[] letters, char letter)
-		{
-			if (letters.Contains(letter)) // checks if the letter is in the char array
-				return true;
-			else
-				return false;
-		}
+        public void AddNewWordToTheGame(string word)
+        {
+            bool containsDigit = word.Any(char.IsDigit); // the word must not have any digits
 
-		private void ShowWordWithFirstLetter(string word)
-		{
-			for (int i = 0; i < word.Length; i++)
-			{ // shows only the first letter of the word
-				if (i == 0)
-					Console.Write($"{word[i]} ");
-				else 
-					Console.Write("_ "); // other letters are "empty"
-			}
+            if (containsDigit == false && word.Length >= 3) // the word must be at least 3 letters long to be considered as a valid word
+                this._words.ListOfWords.Add(word);
+        }
 
-			Console.WriteLine();
-		}
+        private bool CheckForContainingLetters(char[] letters, char letter)
+        {
+            if (letters.Contains(letter)) // checks if the letter is in the char array
+                return true;
+            else
+                return false;
+        }
 
-		private void ShowProgressOfWord(string word)
-		{
-			for (int i = 0; i < word.Length; i++)
-			{
-				if (word[i] != '_')
-					Console.Write($"{word[i]} ");
-				else
-					Console.Write("_ ");
-			}
+        private void ShowWordWithFirstLetter(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            { // shows only the first letter of the word
+                if (i == 0)
+                    Console.Write($"{word[i]} ");
+                else
+                    Console.Write("_ "); // other letters are "empty"
+            }
 
-			Console.WriteLine();
-		}
+            Console.WriteLine();
+        }
 
-		private string WordInProgress(char[] letters, char letter, StringBuilder builder)
-		{
-			int indexOfLetter = Array.IndexOf(letters, letter);
+        private void ShowProgressOfWord(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != '_')
+                    Console.Write($"{word[i]} ");
+                else
+                    Console.Write("_ ");
+            }
 
-			if (builder.Length == letters.Length)
-			{ // if the builder has a length of the array of chars
-				for (int i = 0; i < builder.Length; i++)
-				{ // it will loop through the builder and find the index of the letter
-					if (builder[i] == '_')
-					{
-						if (i == indexOfLetter)
-						{
-							builder.Replace('_', letter, indexOfLetter, 1); // replaces the letter
-						}
+            Console.WriteLine();
+        }
 
-						if (i == indexOfLetter + 1 && letters[indexOfLetter + 1] == letter)
-						{
-							builder.Replace('_', letter, indexOfLetter + 1, 1); // replaces the letter
-						}
-					}
-				}
-			}
-			else
-			{// for the first time assigning the builder to the word(char array) it won't have the length of the word
-				for (int i = 0; i < letters.Length; i++)
-				{  // makes a new stringbuilder
-					if (i == indexOfLetter)
-						builder.Append(letter);
-					else if (i == 0)
-						builder.Append(letters[0]);
-					else
-						builder.Append('_');
-				}
-			}
+        private string WordInProgress(char[] letters, char letter, StringBuilder builder)
+        {
+            int indexOfLetter = Array.IndexOf(letters, letter);
 
-			return builder.ToString();
-		}
+            if (builder.Length == letters.Length)
+            { // if the builder has a length of the array of chars
+                for (int i = 0; i < builder.Length; i++)
+                { // it will loop through the builder and find the index of the letter
+                    if (builder[i] == '_')
+                    {
+                        if (i == indexOfLetter)
+                        {
+                            builder.Replace('_', letter, indexOfLetter, 1); // replaces the letter
+                        }
 
-		private int GetRepeatedLetters(char[] letters, char letter)
-		{
-			int count = 0;
+                        if (i == indexOfLetter + 1 && letters[indexOfLetter + 1] == letter)
+                        {
+                            builder.Replace('_', letter, indexOfLetter + 1, 1); // replaces the letter
+                        }
+                    }
+                }
+            }
+            else
+            {// for the first time assigning the builder to the word(char array) it won't have the length of the word
+                for (int i = 0; i < letters.Length; i++)
+                {  // makes a new stringbuilder
+                    if (i == indexOfLetter)
+                        builder.Append(letter);
+                    else if (i == 0)
+                        builder.Append(letters[0]);
+                    else
+                        builder.Append('_');
+                }
+            }
 
-			for (int i = 0; i < letters.Length; i++)
-			{
-				if (letters[i] == letter)
-					count++;
-			}
+            return builder.ToString();
+        }
 
-			return count;
-		}
-	}
+        private int GetRepeatedLetters(char[] letters, char letter)
+        {
+            int count = 0;
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] == letter)
+                    count++;
+            }
+
+            return count;
+        }
+    }
 }
